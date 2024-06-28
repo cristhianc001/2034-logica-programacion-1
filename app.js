06ca6e36a8685dc82c6dfe358d58ecb55fd7d75f
Cristhian Castro Arias <cristhiancastro001@gmail.com>
2024-06-27 22:35:09 -0500

    primer modulo

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-alert("Hola mundo"); /* pop ups */
+let numeroSecreto = 6;
 let numeroUsuario = prompt("Pon un numero por favor:");
+
+
+/* alert("Hola mundo");  pop ups */
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert("Acertaste el numero");
+}
---
+++ desafio1.js
@@ -0,0 +1,15 @@
+ /* let nombre = "Luna"; */
+ /* let edad = 25; */
+let numeroDeVentas  = 50;
+let saldoDisponible  = 100;
+let mensajeDeError  = "¡Error! Completa todos los campos";
+
+let nombre = prompt("Cual es tu nombre?");
+let edad = prompt("Cual es tu edad?");
+
+if (edad >= 18) {
+    alert("¡Puedes obtener tu licencia de conducir!");
+} 
+
+
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
