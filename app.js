c38f8cbfdeefac564eb8dcd2826926eb31c2bc81
Cristhian Castro Arias <cristhiancastro001@gmail.com>
2024-06-30 19:47:24 -0500

    3er modulo

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-// variables
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Pon un numero por favor:");
+let numeroSecreto = 5;
+let numeroUsuario;
+let intentos=0;  
+let palabraIntento= "intento"; // si es 1 intento. Si es mas de 1, la variable a cambia. Ver el else.
+
+while(numeroUsuario != numeroSecreto) {
+    
+    numeroUsuario = prompt("Pon un numero por favor:");
 
 
-/* alert("Hola mundo");  pop ups */
-console.log(numeroUsuario);
+    /* alert("Hola mundo");  pop ups */
+    console.log(numeroUsuario);
+    intentos = intentos+1 
 
-if (numeroUsuario == numeroSecreto) {
-    alert(`Acertaste el numero: ${numeroSecreto}` ); // uso de comilla invertida
-}
+    if (numeroUsuario == numeroSecreto) {
+        alert(`Acertaste el numero: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraIntento}.` ); // uso de comilla invertida
+    }
+    else {
+        if (numeroUsuario > numeroSecreto) {
+            alert("El numero secreto es menor");
+        } else{
+            alert("El numero secreto es mayor");
+        }
+        
+        palabraIntento = "intentos"; // si hay equivocacion, ahora son intentoS, en plural.
 
-else {
-    alert("Numero incorrecto");
+        // alert("Numero incorrecto");
+    }
+
 }
